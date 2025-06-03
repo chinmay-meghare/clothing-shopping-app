afce4f8e79c17da833626a1c40a9acd021950652
chinmay-meghare <chinmaym100@gmail.com>
2025-06-04 00:02:00 +0530

    Initial commit of the complete clothing-shopping-app project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2 className="bold bg-red-400">
+        {" "}
+        Clothing Shopping App - Chinmay Meghare
+      </h2>
+    </>
+  );
+}
+
+export default App;
