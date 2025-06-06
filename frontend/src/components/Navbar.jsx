38f4aaeda3d727a571ebe1b537d65c0d41c16615
chinmay-meghare <chinmaym100@gmail.com>
2025-06-06 23:56:26 +0530

    feat: add Navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-center py-5 font-medium'>
+
+    </div>
+  )
+}
+
+export default Navbar
+08668244898
+
+9975998195
