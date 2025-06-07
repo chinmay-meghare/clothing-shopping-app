4301ef098c689d3a9b37a572b24d8a6c94a374d0
chinmay-meghare <chinmaym100@gmail.com>
2025-06-07 23:58:41 +0530

    feat: finish Navbar component with styles and layout tweaks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"></div>
 
+<Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collection />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/collection" element={<Collection />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/placeholder" element={<Placeholder />} />
-        <Route path="/orders" element={<Orders />} />
+        <Route path="/orders" element={<Orders />} /> */}
       </Routes>
     </>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-center py-5 font-medium'>
-
+     <img src={assets.logo} /> 
     </div>
   )
 }
 
 export default Navbar
-08668244898
-
-9975998195
