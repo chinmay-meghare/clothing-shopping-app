c55cf6d7a58fd62394b0024207d7607d791d160b
chinmay-meghare <chinmaym100@gmail.com>
2025-06-10 23:57:13 +0530

    chore: added assets and title

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,13 +1,17 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const LatestCollection = () => {
-
-    const {products} = useContext(ShopContext)
+  const { products } = useContext(ShopContext);
 
   return (
-    <div>LatestCollection</div>
-  )
-}
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+      </div>
+    </div>
+  );
+};
 
-export default LatestCollection
+export default LatestCollection;
