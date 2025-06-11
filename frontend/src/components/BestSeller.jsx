2ca81af0daa85d2a3eb753e2d1392b6d9d82a3b9
chinmay-meghare <chinmaym100@gmail.com>
2025-06-12 01:28:54 +0530

    feat: create BestSeller section and integrate into Home page

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,33 +1,33 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { ShopContext } from "../context/ShopContext";
+import { useState } from "react";
 import Title from "./Title";
 import ProductItem from "./ProductItem";
 
-const LatestCollection = () => {
+const BestSeller = () => {
   const { products } = useContext(ShopContext);
 
-  const [latestProducts, setLatestProducts] = useState([]);
+  const [bestSeller, setBestSeller] = useState([]);
 
   useEffect(() => {
-    setLatestProducts(products.slice(0, 10));
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
   }, []);
 
   return (
     <div className="my-10">
-      <div className="text-center py-8 text-3xl">
-        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Shop now from our refreshing collection
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
+          dolores?
         </p>
       </div>
-
-      {/* Rendering Products */}
-
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProducts.map((item, index) => (
+        {bestSeller.map((item, index) => (
           <ProductItem
             key={index}
-            id={item._id}
+            id={item.id}
             image={item.image}
             name={item.name}
             price={item.price}
@@ -38,4 +38,4 @@ const LatestCollection = () => {
   );
 };
 
-export default LatestCollection;
+export default BestSeller;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,7 +18,9 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" alt="website-logo" />
+      <Link to="/">
+        <img src={assets.logo} className="w-36" alt="website-logo" />
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <li>
