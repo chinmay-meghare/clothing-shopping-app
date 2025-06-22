082f2eb2cc2dfd7473ac257f911890858209b539
chinmay-meghare <chinmaym100@gmail.com>
2025-06-22 14:35:37 +0530

    feat: added image selection functionality in Product.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import { assets } from "../assets/assets";
 import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
-  const { products,search, showSearch  } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -30,8 +30,10 @@ const Collection = () => {
   const applyFilter = () => {
     let productsCopy = products.slice();
 
-    if (showSearch && search){
-      productsCopy = productsCopy.filter(item=> item.name.toLowercase().includes(search.toLowercase()))
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      );
     }
 
     if (category.length > 0) {
@@ -73,7 +75,7 @@ const Collection = () => {
 
   useEffect(() => {
     applyFilter();
-  }, [category, subCategory]);
+  }, [category, subCategory, search, showSearch]);
 
   useEffect(() => {
     sortProduct();
