56f73125bc4690946c0323247fe4e7c02057871f
chinmay-meghare <chinmaym100@gmail.com>
2025-06-26 23:37:56 +0530

    feat: begin Add to Cart logic in Product page (not functional yet)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import Footer from "./components/Footer";
 import Collection from "./pages/Collection";
 import SearchBar from "./components/SearchBar";
 import Product from "./pages/Product";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   return (
     <>
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <ToastContainer />
         <Navbar />
         <SearchBar />
         <Routes>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,5 @@
 // to store all common variables and state variables in one place
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
@@ -8,7 +8,28 @@ const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
-  const [showSearch, setShowSearch] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);  
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = async (itemId,size) => {
+    // to copy cartItems. used structuredClone(as it is a object)
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {g
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData)
+  };
+
+  useEffect(()=> {console.log(cartItems);
+  }, [cartItems])
 
   const value = {
     products,
@@ -18,6 +39,7 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
+    cartItems,addToCart
   };
 
   return (
