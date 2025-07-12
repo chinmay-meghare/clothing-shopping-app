496af5df57bdeeade155bd11cf4b09a104eb8b07
chinmay-meghare <chinmaym100@gmail.com>
2025-07-12 22:27:36 +0530

    fix: changed BrowserRouter to HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/clothing-shopping-app">
+  <HashRouterRouter basename="/clothing-shopping-app">
     <ShopContextProvider>
-      {/* <StrictMode> */}
+      <StrictMode>
       <App />
-      {/* </StrictMode> */}
+      </StrictMode>
     </ShopContextProvider>
-  </BrowserRouter>
+  </HashRouterRouter>
 );
