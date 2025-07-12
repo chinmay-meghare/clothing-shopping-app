c210f79469e3752132d9bce9af1322b091d84e6a
chinmay-meghare <chinmaym100@gmail.com>
2025-07-12 23:57:46 +0530

    git commit -m Final Fix: Removed basename from BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/clothing-shopping-app">
+  <BrowserRouter >
     <ShopContextProvider>
       <StrictMode>
       <App />
